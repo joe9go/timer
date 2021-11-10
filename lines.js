6e56e402c2a97a28c70201398a9c7e793ae58bee
joe9go <38994948+joe9go@users.noreply.github.com>
2021-11-10 18:47:02 -0500

    Add files via upload

---
+++ lines.js
@@ -0,0 +1,29 @@
+var dict = {};
+
+function dothing(){
+    today = new Date();
+    time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    if(textIn.value in dict){
+        dict[textIn.value][1] = time;
+    }
+    else{
+        dict[textIn.value] = [time,0];
+    }
+}
+
+function printthing(){
+    totout = "";
+    for(i in dict){
+        totout += String(dict[i])+"\n";
+    }
+    out.textContent = totout;
+}
+
+textIn = document.getElementById("the text")
+button = document.getElementById("the button")
+endbutton = document.getElementById("complete")
+out = document.getElementById("out")
+
+button.onclick = dothing;
+endbutton.onclick = printthing;
+textIn.addEventListener('submit',dothing);
